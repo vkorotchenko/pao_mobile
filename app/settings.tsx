385f4a3fe206303907d372d4ca64ed847bb3415a
Vadim Korotchenko <vkorotchenko@microsoft.com>
2023-06-19 09:56:56 -0700

    refactor dirs

---
+++ app/settings.tsx
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import { Platform, StyleSheet } from 'react-native';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+import Ble from "../components/Ble";
+
+export default function ModalScreen() {
+  return (
+    <View style={styles.container}>
+      {/*<Ble /> // TODO need to create a screen which will connect to the Bluetooth*/}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
